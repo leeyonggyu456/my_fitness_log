7ff7cc2eb0b367d225ddcd31c8c1e0529984242c
Lee Yong Gyu <yonggyulee6655@gmail.com>
2025-11-24 18:09:54 +0900

    Add /add route for inserting workout records

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, redirect
 import sqlite3
 import os
 
@@ -24,7 +24,38 @@ def init_db():
     conn.commit()
     conn.close()
 
+@app.route('/add', methods=['GET','POST'])
+def add_record():
+    if request.method == 'POST':
+        date = request.form['date']
+        exercise_name = request.form['exercise_name']
+        sets = request.form['sets']
+        reps = request.form['reps']
+        weight = request.form['weight']
 
+        conn = sqlite3.connect(DB_NAME)
+        cur = conn.cursor()
+        cur.execute(
+            "INSERT INTO Workout (date, exercise_name, sets, reps, weight) VALUES (?, ?, ?, ?, ?)",
+            (date, exercise_name, sets, reps, weight)
+        )
+        conn.commit()
+        conn.close()
+
+        return redirect('/')
+    
+    return '''
+        <h2>운동 기록 추가</h2>
+        <form method="post">
+            날짜: <input type="text" name="date"><br>
+            운동명: <input type="text" name="exercise_name"><br>
+            세트 수: <input type="number" name="sets"><br>
+            반복 수: <input type="number" name="reps"><br>
+            무게(kg): <input type="number" step="0.1" name="weight"><br>
+            <input type="submit" value="저장">
+        </form>
+        <p><a href="/">홈으로</a></p>
+    '''
 
 
 @app.route('/')
