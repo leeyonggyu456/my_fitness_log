4041111ae6eb00872af68581a4be1a30559bfe0f
Lee Yong Gyu <yonggyulee6655@gmail.com>
2025-11-24 18:01:43 +0900

    Add SQLite database initialization

--- app.py
+++ app.py
@@ -1,11 +1,37 @@
 from flask import Flask
+import sqlite3
+import os
 
 app = Flask(__name__)
 
+DB_NAME = 'workout.db'
+
+
+#DB 초기화 함수
+def init_db():
+    conn = sqlite3.connect(DB_NAME)
+    cur = conn.cursor()
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS Workout (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            date TEXT,
+            exercise_name TEXT,
+            sets INTEGER,
+            reps INTEGER,
+            weight REAL
+        )
+    ''')
+    conn.commit()
+    conn.close()
+
+
+
+
 @app.route('/')
 def home():
-    return "Hello, My Fitness Log!"
+    return "My Fitness Log - DB Initialized!"
 
 
 if __name__ == '__main__':
+    init_db()
     app.run(debug=True)
